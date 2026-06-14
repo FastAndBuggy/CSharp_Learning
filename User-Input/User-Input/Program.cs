8920934f4af3f875ce14692cfe9d6cc668a32cd4
NotKingDavid <117505029+FastAndBuggy@users.noreply.github.com>
2026-06-13 21:46:43 -0600

    Add files via upload

---
+++ User-Input/User-Input/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Hello, World!");
+
+// ----- //
+
+Console.WriteLine("Enter age:");
+
+String userAge = Console.ReadLine();
+
+Console.WriteLine("Enter your name:");
+
+String userName = Console.ReadLine();
+
+Console.WriteLine("Username: " +  userName);
+Console.WriteLine("UserAge: " +  userAge);
