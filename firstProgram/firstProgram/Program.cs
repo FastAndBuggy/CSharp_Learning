9f5ecb9ab5a49abee8c5758be6aa7e3fb04b8038
NotKingDavid <117505029+FastAndBuggy@users.noreply.github.com>
2026-06-13 21:31:51 -0600

    Add files via upload

---
+++ firstProgram/firstProgram/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Hello, World!");
+
+// --------- //
+// type variableName = value;
+String name = "John Doe";
+int birth = 2009;
+int age = 17;
+
+Console.WriteLine("Hello im " + name + " im borned in " + birth + " and im " + age + " years old.");
+
+// end 
